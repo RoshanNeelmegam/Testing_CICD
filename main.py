141beeedb92f3b8a527ae3255a5e5ce6c3dfcb3c
Roshan Neelmegam <roshan@arista.com>
2025-06-07 10:19:19 +0530

    Test3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def greet():
-    return "Testing CI/CD"
+    return "Testing CI/CD 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("4440"), debug=True)
